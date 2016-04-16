469f00510a54fd966a4b6bfb82eb159a6c27dabf
Felipe Paiva <fe.pa.mi@gmail.com>
2016-04-16 00:08:11 -0300

    Tudo certo

--- modulos/promoter/web/controllers/promoterCtrl.js
+++ modulos/promoter/web/controllers/promoterCtrl.js
@@ -20,17 +20,7 @@
                     return;
                 }
 
-                var promoter = new promoterSrvc.Promoter();
-                promoter.nome = newPromoter.nome;
-                promoter.email = newPromoter.email;
-                promoter.senha = newPromoter.senha;
-                promoter.especialidade = newPromoter.especialidade;
-                promoter.experiencia = newPromoter.experiencia;
-                promoter.biografia = newPromoter.biografia;
-                promoter.dataNascimento = newPromoter.dataNascimento;
-
-                //TODO: Save
-                promoter.$save(function (promoter, putResponseHeaders) {
+                promoterSrvc.novoPromoter(newPromoter).then(function (resp) {
                     $scope.showError = false;
                     $scope.newPromoter = {};
                     $scope.message = "Usuário Cadastrado com sucesso";
@@ -42,6 +32,7 @@
 
     function promoterCtrl($scope, $location, promoterSrvc) {
         // No controller, coloque tudo relacionado ao Scope. Em geral são variaveis ou açoes de click
+        localStorage.removeItem('token');
         $scope.showError = false;
         $scope.promoter = {};
         $scope.message = ""
@@ -64,98 +55,103 @@
     function perfilPromoterCtrl($scope, $location, promoterSrvc) {
         // No controller, coloque tudo relacionado ao Scope. Em geral são variaveis ou açoes de click
         $scope.showError = false;
-        $scope.newPromoter = {
-            nome: "Felipe",
-            email: "admin@admin.com",
-            especialidade: "Festas",
-            experiencia: "Muitas Festas",
-            biografia: "Varias Festas",
-            dataNascimento: new Date(99,5,24)
-        };
-        $scope.message = ""
+        $scope.message = "";
+        var oldEmail;
+        promoterSrvc.getPerfil().then(function(promoter){
+            $scope.newPromoter = promoter;
+            oldEmail = promoter.email;
+            $scope.newPromoter.dataNascimento = new Date(promoter.dataNascimento)
+        }, function(err){
+            $scope.message = "Falha ao carregar perfil";
+        });
 
+        var alter = function(newPromoter){
+            var promoter = new promoterSrvc.Promoter();
+            Object.keys(newPromoter).map(function(value, index) {
+                promoter[value] = newPromoter[value];
+            });
+
+            promoter.$update({id: newPromoter.id}, function (updatedPromoter, putResponseHeaders) {
+                oldEmail = updatedPromoter.email;
+                $scope.showError = false;
+                $scope.message = "Usuário alterado com sucesso";
+                console.log('promoter altered with success');
+            });
+        };
+        
         $scope.alterPromoter = function (isvalid, newPromoter) {
             if(!isvalid){
                 $scope.showError = true;
                 $scope.message = "Ainda existem campos inválidos";
                 return;
             }
+            if(oldEmail == newPromoter.email){
+                alter(newPromoter);
+            }else{
+                promoterSrvc.isEmailTaken(newPromoter.email).then(function (resp) {
+                    if(resp.data.length != 0){
+                        $scope.showError = true;
+                        $scope.message = "E-mail já cadastrado";
+                    }else{
+                        alter(newPromoter);
+                    }
+                });
+            }
+        };
 
-            promoterSrvc.isEmailTaken(newPromoter.email).then(function (resp) {
-                if(resp.data.length != 0){
-                    $scope.showError = true;
-                    $scope.message = "E-mail já cadastrado";
-                    return;
-                }
-
-                var promoter = new promoterSrvc.Promoter();
-                promoter.nome = newPromoter.nome;
-                promoter.email = newPromoter.email;
-                promoter.senha = newPromoter.senha;
-                promoter.especialidade = newPromoter.especialidade;
-                promoter.experiencia = newPromoter.experiencia;
-                promoter.biografia = newPromoter.biografia;
-                promoter.dataNascimento = newPromoter.dataNascimento;
-
-                //TODO: alter
-                // promoter.$save(function (promoter, putResponseHeaders) {
-                    $scope.showError = false;
-                    $scope.message = "Usuário alterado com sucesso";
-                    console.log('promoter altered with success');
-                // });
+        $scope.removePromoter = function () {
+            var promoter = new promoterSrvc.Promoter();
+            promoter.$remove({id: $scope.newPromoter.id}, function () {
+                $location.path('/promoter');
             });
         };
     };
+
     function fornecedoresPromoterCtrl($scope, $location, promoterSrvc) {
         // No controller, coloque tudo relacionado ao Scope. Em geral são variaveis ou açoes de click
-        $scope.showError = false;
         $scope.message = ""
         $scope.onlyFavorito = false;
-        var fornecedores = [
-        {
-            nome: "Fornecedor Buffet",
-            servico: "Buffet",
-            preco: 1000,
-            favorito: false
-        },
-        {
-            nome: "Fornecedor Filmagem",
-            servico: "Filmagem",
-            preco: 2000,
-            favorito: true
-        }];
-        $scope.fornecedores = fornecedores;
-
+        var fixedFornecedors = []
+        promoterSrvc.getFornecedors().then(function(fornecedors){
+            fixedFornecedors = fornecedors;
+            $scope.fornecedors = fornecedors;
+        }, function(err){
+            $scope.message = "Falha ao carregar perfil";
+        });
+        $scope.changeFavorito = function(fornecedor) {
+            if(fornecedor.favorito){
+                promoterSrvc.addToFavoritos(fornecedor)
+            }
+            else{
+                promoterSrvc.removeFromFavoritos(fornecedor)
+            }
+        }
         $scope.filterFavorito = function () {
             if($scope.onlyFavorito){
-                $scope.fornecedores = fornecedores.filter(function(value){
+                $scope.fornecedors = fixedFornecedors.filter(function(value){
                     return value.favorito
                 });
             }else{
-                $scope.fornecedores = fornecedores;
+                $scope.fornecedors = fixedFornecedors;
             }
         };
     };
+
     function servicosPromoterCtrl($scope, $location, promoterSrvc) {
         // No controller, coloque tudo relacionado ao Scope. Em geral são variaveis ou açoes de click
-        $scope.showError = false;
         $scope.message = ""
-        $scope.servicos = [
-        {
-            nome: "Buffet do fornecedor de Buffet",
-            fornecedor: "Fornecedor de Buffet",
-            estado: "Orçamento pedido",
-            orcamento: 0
-        },
-        {
-            nome: "Filmagem do fornecedor de Filmagem",
-            fornecedor: "Fornecedor de Filmagem",
-            estado: "Orçamento mandado",
-            orcamento: 2000
-        }];
+        promoterSrvc.getServicos().then(function(servicos){
+            $scope.servicos = servicos;
+        }, function(err){
+            $scope.message = "Falha ao carregar perfil";
+        });
 
         $scope.cancel = function (servico) {
-            servico.estado = "Cancelado"
+            promoterSrvc.cancelServico(servico).then(function (newServico){
+                Object.keys(newServico).map(function(value) {
+                    servico[value] = newServico[value];
+                });
+            });
         };
     };
 
--- modulos/promoter/web/services/promoterSrvc.js
+++ modulos/promoter/web/services/promoterSrvc.js
@@ -3,10 +3,13 @@
     function promoterSrvc($resource, $http, $q) {
         // no service coloque as chamadas para nosso api.
         var Promoter = $resource('/rest/promoters/:id', {}, {
-        save: {
-            method: 'POST',
-            headers: { 'x-access-token': localStorage.token }
-        }});
+            get:    {method:'GET', headers: { 'x-access-token': localStorage.token }},
+            save:   {method:'POST', headers: { 'x-access-token': localStorage.token }},
+            update:   {method:'PUT', headers: { 'x-access-token': localStorage.token }},
+            query:  {method:'GET', headers: { 'x-access-token': localStorage.token }, isArray:true},
+            remove: {method:'DELETE', headers: { 'x-access-token': localStorage.token }},
+            delete: {method:'DELETE', headers: { 'x-access-token': localStorage.token }} 
+        });
 
         var isEmailTaken = function (email) {
             var def = $q.defer();
@@ -27,9 +30,85 @@
                     def.reject(data.message);
                     return;
                 }
-                console.log(data.token)
                 localStorage.token = data.token;
                 def.resolve([]);
+            }, function () {
+                console.error('Erro de Servidor!');
+                def.reject([]);
+            });
+            return def.promise;
+        };
+
+        var novoPromoter = function (promoter) {
+            var def = $q.defer();
+            $http.post('/api/novoPromoter/', promoter).then(function (resp) {
+                def.resolve([]);
+            }, function () {
+                console.error('Erro de Servidor!');
+                def.reject([]);
+            });
+            return def.promise;
+        };
+
+        var getPerfil = function () {
+            var def = $q.defer();
+            $http.get('/api/loadPerfil/', {headers: { 'x-access-token': localStorage.token }}).then(function (resp) {
+                def.resolve(resp.data);
+            }, function () {
+                console.error('Erro de Servidor!');
+                def.reject([]);
+            });
+            return def.promise;
+        };
+
+        var getFornecedors = function () {
+            var def = $q.defer();
+            $http.get('/api/loadFornecedors/', {headers: { 'x-access-token': localStorage.token }}).then(function (resp) {
+                def.resolve(resp.data);
+            }, function () {
+                console.error('Erro de Servidor!');
+                def.reject([]);
+            });
+            return def.promise;
+        };
+
+        var getServicos = function () {
+            var def = $q.defer();
+            $http.get('/api/loadServicos/', {headers: { 'x-access-token': localStorage.token }}).then(function (resp) {
+                def.resolve(resp.data);
+            }, function () {
+                console.error('Erro de Servidor!');
+                def.reject([]);
+            });
+            return def.promise;
+        };
+
+        var addToFavoritos = function (fornecedor) {
+            var def = $q.defer();
+            $http.post('/api/addToFavoritos/', {fornecedorId: fornecedor.id}, {headers: { 'x-access-token': localStorage.token }}).then(function (resp) {
+                def.resolve();
+            }, function () {
+                console.error('Erro de Servidor!');
+                def.reject([]);
+            });
+            return def.promise;
+        };
+
+        var removeFromFavoritos = function (fornecedor) {
+            var def = $q.defer();
+            $http.post('/api/removeFromFavoritos/', {fornecedorId: fornecedor.id}, {headers: { 'x-access-token': localStorage.token }}).then(function (resp) {
+                def.resolve();
+            }, function () {
+                console.error('Erro de Servidor!');
+                def.reject([]);
+            });
+            return def.promise;
+        };
+
+        var cancelServico = function (servico) {
+            var def = $q.defer();
+            $http.post('/api/cancelServico/', {servicoId: servico.id}, {headers: { 'x-access-token': localStorage.token }}).then(function (resp) {
+                def.resolve(resp.data);
             }, function () {
                 console.error('Erro de Servidor!');
                 def.reject([]);
@@ -40,7 +119,14 @@
         return {
             isEmailTaken: isEmailTaken,
             Promoter: Promoter,
-            login: login
+            login: login,
+            novoPromoter: novoPromoter,
+            getPerfil: getPerfil,
+            getFornecedors: getFornecedors,
+            getServicos: getServicos,
+            addToFavoritos: addToFavoritos,
+            removeFromFavoritos: removeFromFavoritos,
+            cancelServico: cancelServico
         };
     }
 
